8b0b4bd6d4cb0743912d7e6b2f5fde60d255e4af
Azhar Khan <admin@azhark.com>
2022-06-09 21:28:32 +0530

    add Posts route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/blog', [PostsController::class, 'index'])->name('posts.index');
+Route::get('/blog/{post:slug}', [PostsController::class, 'show'])->name('posts.show');
